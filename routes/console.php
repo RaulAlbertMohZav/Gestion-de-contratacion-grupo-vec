7494c6b0eee2c816e43530ca43f1a068c8268fb0
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2022-08-08 16:22:25 -0500

    Añadiendo 2 nuevos comandos personalizados de artisan

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+Artisan::command('vec:clear', function () {
+    $this->call('cache:clear');
+    $this->call('config:clear');
+    $this->call('event:clear');
+    /*$this->call('queue:clear');*/
+    $this->call('config:cache');
+    $this->call('optimize:clear');
+    $this->call('optimize');
+})->purpose('Optimiza la caché de la APP');
+
+
+Artisan::command('vec:migrate', function () {
+    $this->call('migrate:fresh');
+    $this->call('db:seed');
+    $this->call('vec:clear');
+})->purpose('Borra tablas, realiza la migracion, ejecuta los seeders y optimiza la cache de la app');
